4985cd0cbf61f7dd099d200d54cb3b6a3ccddf39
blazefi <75904135+blazefi@users.noreply.github.com>
2021-05-02 17:43:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import flask
+from flask import Flask, jsonify, request, render_template
+import json
+import numpy as np
+import pickle
+
+
+best_model = pickle.load(open('./models/best_model.sav', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():   
+    #'rating','net_experience', 'jr', 'senior', 'bachelor', 'masters', 'posting_frequency'
+     
+    x_in = [int(x) for x in request.form.values()]
+    x_in = np.array(x_in).reshape(1,-1)
+
+    pred = best_model.predict(x_in)
+    
+    prediction = round(np.exp(pred), 2)
+    
+    return render_template('index.html', prediction_text='Your predicted salary is {}'.format(prediction))
+
+
+if __name__ == '__main__':
+ app.run(debug=True)
